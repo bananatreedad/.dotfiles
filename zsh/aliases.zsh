a93ae3675fefc5c37ddabce9a105ed867c799f21
bananatreedad <jnt.meier@gmail.com>
2016-04-04 23:17:20 +0200

    add bunch of new functions and scripts

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -13,3 +13,5 @@ alias du='du -h -c' # calculate disk usage for a folder
 alias ip="dig +short myip.opendns.com @resolver1.opendns.com"
 alias localip="ipconfig getifaddr en1"
 alias ips="ifconfig -a | perl -nle'/(\d+\.\d+\.\d+\.\d+)/ && print $1'"
+
+alias slp="pmset sleepnow"
