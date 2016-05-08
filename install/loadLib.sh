f464f58798c054bc02162e261aced6a8a3527278
bananatreedad <jnt.meier@gmail.com>
2016-05-08 11:58:24 +0200

    add source of code and do the link anyway

--- install/loadLib.sh
+++ install/loadLib.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 
 #install gh script from jasonmccreary
+#see https://github.com/jasonmccreary/gh
 
 if [ ! -f ~/.dotfiles/bin/gh.sh ]; then
     wget -O ~/.dotfiles/bin/gh.sh https://raw.githubusercontent.com/jasonmccreary/gh/master/gh.sh
     chmod +x ~/.dotfiles/bin/gh.sh
 else
-    echo "gh.sh exists already."
+    echo "gh.sh exists already - no download needed."
 fi
 
 ln -s ~/.dotfiles/bin/gh.sh /usr/local/bin/gh
